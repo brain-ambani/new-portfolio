b4c1949675aed44fa663b02965beb8de32245259
brian-ambani <brainambani1@gmail.com>
2024-05-17 13:18:54 +0300

    updated the header

--- app/sections/Header.js
+++ app/sections/Header.js
@@ -1,14 +1,13 @@
 function Header() {
   return (
     <header className="w-3/4 mx-auto mt-24">
-      <div>Fullstack Developer</div>
-      <div>
-        <h1>
-          Building the Future
-          <br />
-          My hourney as a developer and a founder
-        </h1>
-      </div>
+      <h1 className="text-4xl mb-6 text-zinc-400">Fullstack Developer</h1>
+
+      <h1 className="text-2xl">
+        Building the Future
+        <br />
+        <span>My hourney as a developer and a founder</span>
+      </h1>
       <div>
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
